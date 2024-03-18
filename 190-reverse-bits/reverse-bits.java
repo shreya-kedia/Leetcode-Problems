5acfd7699ede4cd0b2846c1da62c6c45692af6a7
Shreya Kedia <54714972+shreya-kedia@users.noreply.github.com>
2024-03-18 17:54:52 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (97.67%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,8 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        //System.out.println(Integer.reverse(n));
+
+        return Integer.reverse(n);
+    }
+}
