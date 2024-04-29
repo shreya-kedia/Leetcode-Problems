207e90815e8d6c601e4c8c0168efa142cca6deb0
Shreya Kedia <54714972+shreya-kedia@users.noreply.github.com>
2024-04-29 23:38:57 +0530

    Time: 1 ms (99.93%) | Memory: 41.9 MB (63.77%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.java
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -4,9 +4,9 @@ class Solution {
         int next=0;
         int sum=0;
         for(int i=0;i< s.length()-1;i++){
-            curr=(int)s.charAt(i);
-            next=(int)s.charAt(i+1);
-            sum= sum+ Math.abs(curr-next);
+            //curr=(int)s.charAt(i);
+            //next=(int)s.charAt(i+1);
+            sum= sum+ Math.abs((int)s.charAt(i)-(int)s.charAt(i+1));
         }
          return sum;
     }
