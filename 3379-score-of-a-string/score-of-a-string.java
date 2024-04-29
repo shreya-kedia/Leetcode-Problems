99d07c89fcf65c7bf4109c9f6faf3e1db9812429
Shreya Kedia <54714972+shreya-kedia@users.noreply.github.com>
2024-04-29 23:39:11 +0530

    Time: 1 ms (99.93%) | Memory: 42 MB (28.50%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.java
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public int scoreOfString(String s) {
-        int curr=0;
-        int next=0;
+       // int curr=0;
+        //int next=0;
         int sum=0;
         for(int i=0;i< s.length()-1;i++){
             //curr=(int)s.charAt(i);
