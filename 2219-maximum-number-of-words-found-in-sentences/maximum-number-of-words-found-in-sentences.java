41aaeea10b7dbb3d70882892ec3fae073c9e9c82
Shreya Kedia <54714972+shreya-kedia@users.noreply.github.com>
2024-04-30 23:35:58 +0530

    Time: 3 ms (84.71%) | Memory: 44.3 MB (23.01%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
@@ -1,23 +1,26 @@
 class Solution {
     public int mostWordsFound(String[] sentences) {
                 //Stack<char> stack2 = new Stack<char>();
-                int currCount=0;
+                //int currCount=0;
+                int max=0;
                 for(int i=0; i< sentences.length;i++){
                 int spaceCount=0;
-                String sent= sentences[i];
-                int j=0;
-                while(j< sent.length()){
-                    if((sent.charAt(j)+"").equals(" ")){
-                       ++spaceCount;
-                    }
-                    j++;
+                //String sent= sentences[i];
+                //int j=0;
+                // while(j< sent.length()){
+                //     if((sent.charAt(j)+"").equals(" ")){
+                //        ++spaceCount;
+                //     }
+                //     j++;
                     
-                }
-                if(currCount<spaceCount) currCount=spaceCount;
+                // }
+                spaceCount=sentences[i].split(" ").length;
+                max= Math.max(max,spaceCount);
+                //if(currCount<spaceCount) currCount=spaceCount;
 
-                System.out.println(spaceCount);
+                //System.out.println(spaceCount);
                 }
-                return currCount+1;
+                return max;
 
 
         
