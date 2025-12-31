4d53e72f89d7aba4c6d3a7402d9ee843f5cb72bd
Shreya Kedia <54714972+shreya-kedia@users.noreply.github.com>
2025-12-31 15:37:26 +0530

    Time: 1 ms (98.24%) | Memory: 47.8 MB (31.77%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -3,15 +3,16 @@ class Solution {
         if(n==0) return true;
         int m=flowerbed.length;
 
-        for(int i=0;i<m;i++)
-        {
-            if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==m-1 || flowerbed[i+1]==0) ){
+        for(int i=0;i<m;i++){
+            if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==m-1 ||flowerbed[i+1]==0)){
                 flowerbed[i]=1;
                 n--;
                 if(n==0) return true;
             }
+            
         }
+
         return false;
-        
+
     }
 }
